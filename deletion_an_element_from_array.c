357542cd83575408c048b036df9de5bf19d7ff83
Poorvi0906 <poorvirbca24@rvu.edu.in>
2024-11-15 22:45:36 +0530

    Update deletion_an_element_from_array.c

--- deletion_an_element_from_array.c
+++ deletion_an_element_from_array.c
@@ -9,7 +9,7 @@ int main() {
     
     int arr[size];
     
-    // Prompt user to enter the elements of the array
+    // Prompt the user to enter the elements of the array
     printf("Enter %d elements: ", size);
     for(i = 0; i < size; i++) {
         scanf("%d", &arr[i]);
